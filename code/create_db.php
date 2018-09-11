e943c2c09f567d8b66d6602f1c330f2f0e46f0ea
Kampol94 <37142187+Kampol94@users.noreply.github.com>
2018-09-12 00:10:25 +0200

    Update create_db.php

--- code/create_db.php
+++ code/create_db.php
@@ -1,67 +1,60 @@
 <?php
- $servername = "localhost";
+$servername = "localhost";
 $username = "root";
 $password = "mysql";
-@@ -13,13 +14,12 @@
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 // Create database
 $sql = "CREATE DATABASE IF NOT EXISTS Crawler";
 if ($conn->query($sql) === TRUE) {
-    echo "Database created successfully";
     echo "Database created successfully".'<br>';
 } else {
-    echo "Error creating database: " . $conn->error;
     echo "Error creating database: ". $conn->error.'<br>';
 }
- $conn->select_db("Crawler");
- // sql to create table
+$conn->select_db("Crawler");
+// sql to create table
 $sql = "CREATE TABLE IF NOT EXISTS SitesViewed (
 	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-@@ -28,11 +28,10 @@
+	site TEXT,
+	date TIMESTAMP
 )";
- if (mysqli_query($conn, $sql)) {
-    echo "Table SitesViewed created successfully";
+if (mysqli_query($conn, $sql)) {
     echo "Table SitesViewed created successfully".'<br>';
 } else {
-    echo "Error creating table: " . mysqli_error($conn);
     echo "Error creating table: " . mysqli_error($conn).'<br>';
 }
- // sql to create table
+// sql to create table
 $sql = "CREATE TABLE IF NOT EXISTS SitesAwaiting (
 	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-@@ -41,31 +40,28 @@
+	site VARCHAR(200),
+	date TIMESTAMP
 )";
- if (mysqli_query($conn, $sql)) {
-    echo "Table SitesAwaiting created successfully";
+if (mysqli_query($conn, $sql)) {
     echo "Table SitesAwaiting created successfully".'<br>';
 } else {
-    echo "Error creating table: " . mysqli_error($conn);
     echo "Error creating table: " . mysqli_error($conn).'<br>';
 }
- $result = mysqli_query($conn, "SHOW COLUMNS FROM `SitesViewed` LIKE 'content'");
+$result = mysqli_query($conn, "SHOW COLUMNS FROM `SitesViewed` LIKE 'content'");
 $exists = (mysqli_num_rows($result)) ? TRUE:FALSE;
- if (!$exists) {
+if (!$exists) {
     // sql to ALTER table
     $sql = "ALTER TABLE SitesViewed ADD content TEXT after site";
-     if (mysqli_query($conn, $sql)) {
-        echo "ALTER TABLE SitesViewed successfully";
+    if (mysqli_query($conn, $sql)) {
         echo "ALTER TABLE SitesViewed successfully".'<br>';
     } else {
-        echo "Error creating table: " . mysqli_error($conn);
-    }
-    
         echo "Error creating table: " . mysqli_error($conn).'<br>';
     }  
     // sql to ALTER table
     $sql = "ALTER TABLE SitesViewed MODIFY site VARCHAR(2048)";
     if (mysqli_query($conn, $sql)) {
-        echo "ALTER TABLE SitesViewed successfully";
         echo "ALTER TABLE SitesViewed successfully".'<br>';
     } else {
-        echo "Error creating table: " . mysqli_error($conn);
         echo "Error creating table: " . mysqli_error($conn).'<br>';
     }
 }
-
 $conn->close();
-
 ?>
