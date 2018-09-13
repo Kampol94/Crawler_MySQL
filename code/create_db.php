47db37e7436eaec8cd7fe6b2bab5c4f50f20b5ef
Kampol94 <37142187+Kampol94@users.noreply.github.com>
2018-09-13 15:43:08 +0200

    Update create_db.php

--- code/create_db.php
+++ code/create_db.php
@@ -1,53 +1,65 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "mysql";
+$password = "";
+
 // Create connection
 $conn = new mysqli($servername, $username, $password);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 // Create database
 $sql = "CREATE DATABASE IF NOT EXISTS Crawler";
 if ($conn->query($sql) === TRUE) {
     echo "Database created successfully".'<br>';
 } else {
-    echo "Error creating database: ". $conn->error.'<br>';
+    echo "Error creating database: " . $conn->error.'<br>';
 }
+
 $conn->select_db("Crawler");
+
 // sql to create table
 $sql = "CREATE TABLE IF NOT EXISTS SitesViewed (
 	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	site TEXT,
 	date TIMESTAMP
 )";
+
 if (mysqli_query($conn, $sql)) {
     echo "Table SitesViewed created successfully".'<br>';
 } else {
     echo "Error creating table: " . mysqli_error($conn).'<br>';
 }
+
 // sql to create table
 $sql = "CREATE TABLE IF NOT EXISTS SitesAwaiting (
 	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	site VARCHAR(200),
 	date TIMESTAMP
 )";
+
 if (mysqli_query($conn, $sql)) {
     echo "Table SitesAwaiting created successfully".'<br>';
 } else {
     echo "Error creating table: " . mysqli_error($conn).'<br>';
 }
+
+
 $result = mysqli_query($conn, "SHOW COLUMNS FROM `SitesViewed` LIKE 'content'");
 $exists = (mysqli_num_rows($result)) ? TRUE:FALSE;
+
 if (!$exists) {
     // sql to ALTER table
     $sql = "ALTER TABLE SitesViewed ADD content TEXT after site";
+
     if (mysqli_query($conn, $sql)) {
         echo "ALTER TABLE SitesViewed successfully".'<br>';
     } else {
         echo "Error creating table: " . mysqli_error($conn).'<br>';
-    }  
+    }
+    
     // sql to ALTER table
     $sql = "ALTER TABLE SitesViewed MODIFY site VARCHAR(2048)";
     if (mysqli_query($conn, $sql)) {
@@ -56,5 +68,7 @@ if (!$exists) {
         echo "Error creating table: " . mysqli_error($conn).'<br>';
     }
 }
+
 $conn->close();
+
 ?>
